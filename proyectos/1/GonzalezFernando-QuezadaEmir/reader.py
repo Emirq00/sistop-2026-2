de3b47262af9bb1e27037376dfc2c94b0b8ab9ee
Emir Quezada <emirquezadao@gmail.com>
2026-05-14 10:33:37 -0600

    Adds directory read :)

--- proyectos/1/GonzalezFernando-QuezadaEmir/reader.py
+++ proyectos/1/GonzalezFernando-QuezadaEmir/reader.py
@@ -33,11 +33,84 @@ def superbloque(data):
 	cluster_size = struct.unpack('<I', data[40:44])[0] 
 	# "<" = Little-endian
     # "I" = Entero 4 bytes (ID)
-    # "H" = Entero 2 bytes (Versión)
-	print(f"Tamaño del cluster: {cluster_size} bytes") # type: ignore
 	# Para obtener el tamaño del cluster, utilizamos struct.unpack con el formato '<I', 
 	# que indica que queremos interpretar los bytes como un entero sin signo de 4 bytes en
 	# formato little-endian. El resultado es una tupla, por lo que accedemos al primer elemento con [0].
+	print(f"Tamaño del cluster: {cluster_size} bytes") 
+
+	num_clusters = struct.unpack('<I', data[50:54])[0]
+	# De manera similar, obtenemos el número de clusters que mide el directorio
+	# utilizando struct.unpack con el mismo formato.
+	print(f"Clusters que mide el directorio: {num_clusters}")
+
+	clusters_unidad = struct.unpack('<I', data[60:64])[0]
+	# Nos indica el número total de clusters que tiene la unidad completa.
+	print(f"Numero de clusters de la unidad completa: {clusters_unidad}")
+
+	return {'id_bytes': id_bytes, 
+		 	'fileName': fileName, 
+			'version': version, 
+			'volumen': volumen, 
+			'cluster_size': cluster_size, 
+			'num_clusters': num_clusters, 
+			'clusters_unidad': clusters_unidad}
+
+def directorio(data, infoSuperbloque):
+	# En esta función leemos el directorio de nuestro fiunamfs.img
+	print("="*60)
+	print("Análisis del Directorio (Clusters 1-8)")
+	print("="*60)
+
+	cluster_size = infoSuperbloque['cluster_size']
+	num_clusters = infoSuperbloque['num_clusters']
+
+	start = cluster_size # Como sabemos que el cluster 0 es el superbloque, 
+						 # el directorio comienza en el cluster 1. 
+	
+	dir_size = num_clusters * cluster_size # Calculamos el tamaño total del directorio.
+
+	end = start + (dir_size) # El directorio ocupa un número de clusters, 
+												# por lo que calculamos el final sumando el tamaño 
+												# total del directorio al inicio.
+	
+	infoDirectorio = data[start:end] # Obtenemos los bytes correspondientes al directorio.
+	entry_size = 64 # Cada entrada del directorio ocupa 64 bytes.
+
+	num_entries=dir_size//entry_size # Calculamos el número de entradas dividiendo 
+									 # el tamaño total del directorio entre el tamaño 
+									 # de cada entrada.
+
+	files= [] # Lista para almacenar la información de los archivos encontrados en el directorio.
+
+	for i in range(num_entries):
+		entry_start = i * entry_size
+		entry_data = infoDirectorio[entry_start:entry_start + entry_size] # Bytes de la entrada actual.
+		file_type = chr(entry_data[0]) # El primer byte indica el tipo de archivo (-, /).
+		if file_type=="-":
+			files.append(entry_data) # Si el tipo de archivo es "-", lo consideramos un archivo regular y 
+									 # lo agregamos a la lista de archivos.
+		
+	for entry_data in files:
+		file_type = chr(entry_data[0]) # El primer byte indica el tipo de archivo (-, /).	
+		file_name = entry_data[1:15].decode('ascii', errors='ignore').rstrip('\x00') # Bytes 1-15 para el nombre del archivo.
+		# Bytes 16-20 para el tamaño del archivo.
+		file_size = struct.unpack('<I', entry_data[16:20])[0] 
+
+		# Bytes 20-23 para el primer cluster del archivo.
+		first_cluster = struct.unpack('<I', entry_data[20:24])[0] 
+
+		# Bytes 24-32 para la fecha de creación.
+		creation_time = entry_data[24:44].decode('ascii', errors='ignore').rstrip('\x00')
+
+		# Bytes 40-55 para la fecha de modificación.
+		modification_time = entry_data[44:64].decode('ascii', errors='ignore').rstrip('\x00')
+		print(f"Archivo: {file_name}")
+		print(f"  Tipo: {file_type}")
+		print(f"  Tamaño: {file_size} bytes")
+		print(f"  Primer cluster: {first_cluster}")
+		print(f"  Fecha de creación: {creation_time}")
+		print(f"  Fecha de modificación: {modification_time}")
+
 
 def analizar(nombre):
 	filePath=Path(nombre)
@@ -62,7 +135,12 @@ def analizar(nombre):
 	# ¿qué es un context manager? Es una estructura que permite gestionar recursos de manera eficiente, 
 	# asegurando que se liberen correctamente, incluso si ocurre un error.
 	with open(nombre, 'rb') as f: # r -> read, b -> binary, rb -> read binary
-		superbloque(f.read())		
+		data = f.read()
+
+	infoSuperbloque = superbloque(data)
+
+	directorio(data, infoSuperbloque)
+	
 
 	print("Analisis completo")
 
